111b4e0701a617cd56fd6976d45d76e3604a5f73
sandrine <sandrinezocca@gmail.com>
2019-06-17 14:59:06 +0200

    first commit

---
+++ doczrc.js
@@ -0,0 +1,10 @@
+export default {
+    title: 'Exemples de code OpenLayers / React',
+    base: '/docs',
+    ignore: ['**/blog/**', 'readme.md'],
+    //menu: ['Getting Started', 'Components'],
+    repository: false,
+    themeConfig: {
+      linesToScrollEditor: 70,
+    },
+  }
