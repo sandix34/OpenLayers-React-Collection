cdedfe014c981f998f79e4b16657c0644dae3b63
sandrine <sandrinezocca@gmail.com>
2019-06-23 16:24:12 +0200

    change lien WFS

--- docs/CarteWFS.jsx
+++ docs/CarteWFS.jsx
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 import Map from 'ol/Map';
 import View from 'ol/View';
 import 'ol/ol.css';
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
 
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
 import VectorLayer from 'ol/layer/Vector';
+
+// Conteneur pour les styles de rendu d'entités vectorielles
+import Style from "ol/style/Style";
+//Définir le style de trait pour les entités vectorielles
+import Stroke from "ol/style/Stroke";
 
  /**
   * -----------------
@@ -29,28 +36,40 @@ class CarteWFS extends Component {
     super(props);
 
     this.state = { 
-        center: [-8233510, 4980620], 
-        zoom: 13 
+        center: [-8908887.277395891, 5381918.072437216], 
+        zoom: 10 
     };
+
+    // Sources de données et couche OpenStreetMap
+    this.osm = new TileLayer({
+      source: new OSM()
+    });
+
 
     // Déclaration de la source de la couche en format WFS 
     this.sourceWFS = new VectorSource({
 	// Chargement du lien WFS en format json
-	url: 'https://map.geomatick.com/geoserver/wfs?service=WFS&' +
-	'version=1.1.0&request=GetFeature&typename=tiger:tiger_roads&' +
-	'outputFormat=application/json',
+	url: 'https://ahocevar.com/geoserver/wfs?service=WFS&' +
+  'version=1.1.0&request=GetFeature&typename=osm:water_areas&' +
+  'outputFormat=application/json&srsname=EPSG:3857&',
 	format: new GeoJSON(),
   serverType: 'geoserver'
     });
 
     // Déclaration de la couche WFS 
     this.vecteurWFS = new VectorLayer({ 
-	source: this.sourceWFS 
+  source: this.sourceWFS,
+  style: new Style({
+    stroke: new Stroke({
+      color: 'rgba(0, 0, 255, 1.0)',
+      width: 2
+    })
+  }) 
     });
 
     this.olmap = new Map({
       target: null,
-      layers: [this.vecteurWFS],
+      layers: [this.osm, this.vecteurWFS],
       view: new View({
         center: this.state.center,
         zoom: this.state.zoom
