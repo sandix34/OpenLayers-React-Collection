98491db5c6ad11cf2974eccebacdbc520e7a0f02
sandrine <sandrinezocca@gmail.com>
2019-07-07 16:08:34 +0200

    ajout commentaire - chargement de fichiers externes

--- docs/VecteurFichier.jsx
+++ docs/VecteurFichier.jsx
@@ -56,7 +56,7 @@ class VecteurFichier extends Component {
 	source: this.sourceGPX,
     });
 
-
+    // Déclaration de la carte
     this.olmap = new Map({
       target: null,
       layers: [
@@ -73,32 +73,12 @@ class VecteurFichier extends Component {
     });
   }
 
-  updateMap() {
-    this.olmap.getView().setCenter(this.state.center);
-    this.olmap.getView().setZoom(this.state.zoom);
-  }
-
   componentDidMount() {
+    // Défini l'id de l'élément dans lequel la carte est rendue.
     this.olmap.setTarget("map7");
-
-    // Listen to map changes
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
-      this.setState({ center, zoom });
-    });
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
-    if (center === nextState.center && zoom === nextState.zoom) return false;
-    return true;
-  }
-
 
   render() {
-    this.updateMap(); // Update map on render?
     return (
       <div id="map7" style={{ width: "100%", height: "360px" }}>
       </div>
