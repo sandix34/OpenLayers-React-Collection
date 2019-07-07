7251d3b636c61fd5c245fba20f3e71dd3574ad83
sandrine <sandrinezocca@gmail.com>
2019-07-07 12:41:18 +0200

    ajout commentaire - projection différente

--- docs/Projection2.jsx
+++ docs/Projection2.jsx
@@ -32,14 +32,15 @@ class Projection2 extends Component {
 
     this.state = { 
         center: [2, 45] , 
-        zoom: 3,
-        projection: getProjection('EPSG:2154') 
+        zoom: 3
     };
 
 
     //déclaration de la projection en EPSG:2154 (Lambert 93)
     this.proj4 =  proj4.defs("EPSG:2154","+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
     this.proj4Register = register(proj4);
+
+    this.projection = getProjection('EPSG:2154');
 
     this.centerTransform = transform(this.state.center,'EPSG:4326', 'EPSG:2154')
 
@@ -51,40 +52,18 @@ class Projection2 extends Component {
         })
       ],
       view: new View({
-        projection: this.state.projection,  
+        projection: this.projection,  
         center: this.centerTransform,
         zoom: this.state.zoom
       })
     });
   }
 
-
-  updateMap() {
-    this.olmap.getView().setCenter(this.centerTransform);
-    this.olmap.getView().setZoom(this.state.zoom);
-  }
-
   componentDidMount() {
     this.olmap.setTarget("map3");
-
-    // Listen to map changes
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
-      this.setState({ center, zoom });
-    });
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
-    if (center === nextState.center && zoom === nextState.zoom) return false;
-    return true;
-  }
-
 
   render() {
-    this.updateMap(); // Update map on render?
     return (
       <div id="map3" style={{ width: "100%", height: "360px" }}>
       </div>
