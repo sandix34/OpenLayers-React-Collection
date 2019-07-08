0d10e3da816a9b8ff58e76599d91c88dd87c7ad6
sandrine <sandrinezocca@gmail.com>
2019-07-08 15:16:54 +0200

    ajout commentaire - fond carte stamen

--- docs/FondCarteStamenToner.jsx
+++ docs/FondCarteStamenToner.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import 'ol/ol.css';
+import React, { Component } from "react"
+import Map from "ol/Map"
+import View from "ol/View"
+import TileLayer from "ol/layer/Tile"
+import "ol/ol.css"
 
-import Stamen from 'ol/source/Stamen';
+import Stamen from "ol/source/Stamen"
 
 /**
  * ---------------------
  * Les fonds de carte OL
  * ---------------------
- * 
+ *
  * On appelle communément fonds de carte,
  * un ensemble de données géographiques compilées
  * qui permettent de situer des vecteurs
  * et/ou des rasters spécifiques.
  * Les fonds de carte ont une résolution spatiale
  * et des informations plus ou moins détaillées.
- * 
+ *
  * OL implémente directement plusieurs d'entre eux dans son API,
  * ainsi on dispose des sources de données tuilées suivantes :
  * BingMaps (avec clé),
@@ -26,68 +26,39 @@ import Stamen from 'ol/source/Stamen';
  * et OpenStreetMap (OSM).
  */
 
- 
-
 class FondCarteStamenToner extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { 
-        center: [0, 0], 
-        zoom: 2 
-    };
+    this.state = {
+      center: [0, 0],
+      zoom: 2,
+    }
 
-    // Stamen Toner 
+    // Stamen Toner
     this.StamenToner = new TileLayer({
-	title: 'Stamen Toner',
-	preload: Infinity,
-	source: new Stamen({
-		layer: 'toner',
-	}),
-	visible: true
-    });  
+      source: new Stamen({
+        layer: "toner",
+      }),
+    })
 
     this.olmap = new Map({
       target: null,
       layers: [this.StamenToner],
       view: new View({
         center: this.state.center,
-        zoom: this.state.zoom
-      })
-    });
-  }
-
-  updateMap() {
-    this.olmap.getView().setCenter(this.state.center);
-    this.olmap.getView().setZoom(this.state.zoom);
+        zoom: this.state.zoom,
+      }),
+    })
   }
 
   componentDidMount() {
-    this.olmap.setTarget("map12");
-
-    // Listen to map changes
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
-      this.setState({ center, zoom });
-    });
+    this.olmap.setTarget("map12")
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
-    if (center === nextState.center && zoom === nextState.zoom) return false;
-    return true;
-  }
-
 
   render() {
-    this.updateMap(); // Update map on render?
-    return (
-      <div id="map12" style={{ width: "100%", height: "360px" }}>
-      </div>
-    );
+    return <div id="map12" style={{ width: "100%", height: "360px" }}></div>
   }
 }
 
-export default FondCarteStamenToner;
+export default FondCarteStamenToner
--- docs/FondCarteStamenWatercolor.jsx
+++ docs/FondCarteStamenWatercolor.jsx
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import 'ol/ol.css';
+import React, { Component } from "react"
+import Map from "ol/Map"
+import View from "ol/View"
+import TileLayer from "ol/layer/Tile"
+import "ol/ol.css"
 
-import Stamen from 'ol/source/Stamen';
+import Stamen from "ol/source/Stamen"
 
 /**
  * ---------------------
  * Les fonds de carte OL
  * ---------------------
- * 
+ *
  * On appelle communément fonds de carte,
  * un ensemble de données géographiques compilées
  * qui permettent de situer des vecteurs
  * et/ou des rasters spécifiques.
  * Les fonds de carte ont une résolution spatiale
  * et des informations plus ou moins détaillées.
- * 
+ *
  * OL implémente directement plusieurs d'entre eux dans son API,
  * ainsi on dispose des sources de données tuilées suivantes :
  * BingMaps (avec clé),
@@ -26,68 +26,39 @@ import Stamen from 'ol/source/Stamen';
  * et OpenStreetMap (OSM).
  */
 
- 
-
 class FondCarteStamenWatercolor extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = { 
-        center: [0, 0], 
-        zoom: 2 
-    };
+    this.state = {
+      center: [0, 0],
+      zoom: 2,
+    }
 
-    // Stamen Watercolor 
+    // Stamen Watercolor
     this.StamenWatercolor = new TileLayer({
-	title: 'Stamen Watercolor',
-	preload: Infinity,
-	source: new Stamen({
-		layer: 'watercolor',
-	}),
-	visible: true
-    });  
+      source: new Stamen({
+        layer: "watercolor",
+      }),
+    })
 
     this.olmap = new Map({
       target: null,
       layers: [this.StamenWatercolor],
       view: new View({
         center: this.state.center,
-        zoom: this.state.zoom
-      })
-    });
-  }
-
-  updateMap() {
-    this.olmap.getView().setCenter(this.state.center);
-    this.olmap.getView().setZoom(this.state.zoom);
+        zoom: this.state.zoom,
+      }),
+    })
   }
 
   componentDidMount() {
-    this.olmap.setTarget("map11");
-
-    // Listen to map changes
-    this.olmap.on("moveend", () => {
-      let center = this.olmap.getView().getCenter();
-      let zoom = this.olmap.getView().getZoom();
-      this.setState({ center, zoom });
-    });
+    this.olmap.setTarget("map11")
   }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    let center = this.olmap.getView().getCenter();
-    let zoom = this.olmap.getView().getZoom();
-    if (center === nextState.center && zoom === nextState.zoom) return false;
-    return true;
-  }
-
 
   render() {
-    this.updateMap(); // Update map on render?
-    return (
-      <div id="map11" style={{ width: "100%", height: "360px" }}>
-      </div>
-    );
+    return <div id="map11" style={{ width: "100%", height: "360px" }}></div>
   }
 }
 
-export default FondCarteStamenWatercolor;
+export default FondCarteStamenWatercolor
